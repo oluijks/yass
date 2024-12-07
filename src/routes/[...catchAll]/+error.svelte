0238460b395f380bb69560a5eb0ade0177e86135
Olaf Luijks <olafluijks@proton.me>
2024-12-07 20:59:22 +0100

    feat: Add catch all route (404)

---
+++ src/routes/[...catchAll]/+error.svelte
@@ -0,0 +1,4 @@
+<div class="flex flex-col items-center justify-center mx-auto min-h-[calc(100vh-10rem)] gap-4">
+  <h1 class="text-7xl lg:text-8xl font-sans font-extralight">404</h1>
+  <h2 class="text-3xl lg:text-5xl">Page Not Found</h2>
+</div>
