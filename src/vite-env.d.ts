962b1a0cda2db14b8b35587d372635955dac0d41
Olaf Luijks <olafluijks@proton.me>
2024-12-07 20:56:08 +0100

    feat: Add package versions from package.json

---
+++ src/vite-env.d.ts
@@ -0,0 +1,3 @@
+declare const __VERSION__: string;
+declare const __SVELTE_VERSION__: string;
+declare const __SVELTEKIT_VERSION__: string;
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  define: {
+    __NAME__: `"${pkg.name}"`,
+    __VERSION__: `"${pkg.version}"`,
+    __SVELTE_VERSION__: `"${sveltePackage.version}"`,
+    __SVELTEKIT_VERSION__: `"${svelteKitPackage.version}"`,
+  },
 });
