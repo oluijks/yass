eca3f702225f45d37a19eea55f9ed5edc4e9dfd5
Olaf Luijks <olafluijks@proton.me>
2024-12-08 22:00:57 +0100

    feat: Add metadata component

---
+++ src/lib/components/layout/metadata.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { siteConfig } from "$lib/config/site.js";
+
+  const title = $derived($page.data.meta?.title ?? siteConfig.meta.title);
+  const keywords = $derived($page.data.meta?.keywords ?? siteConfig.meta.keywords);
+  const description = $derived($page.data.meta?.description ?? siteConfig.meta.description);
+</script>
+
+<svelte:head>
+  <title>{title} :: {siteConfig.name}</title>
+  <meta name="description" content={description} />
+  <meta name="keywords" content={keywords} />
+  <meta name="author" content="oluijks">
+</svelte:head>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,27 +1,15 @@
 <script lang="ts">
-  import { page } from "$app/stores";
   import AppFooter from "$lib/components/layout/app-footer.svelte";
   import AppHeader from "$lib/components/layout/app-header.svelte";
+  import Metadata from "$lib/components/layout/metadata.svelte";
   import { Toaster } from "$lib/components/ui/shadcn/sonner/index.js";
-  import { siteConfig } from "$lib/config/site.js";
   import { ModeWatcher } from "mode-watcher";
   import "../assets/styles/app.css";
 
   const { children } = $props();
-
-  const title = $derived($page.data.meta?.title ?? siteConfig.meta.title);
-  const description = $derived(
-    $page.data.meta?.description ?? siteConfig.meta.description,
-  );
-  const keywords = $derived($page.data.meta?.keywords ?? siteConfig.meta.keywords);
 </script>
 
-<svelte:head>
-  <title>{title} :: {siteConfig.name}</title>
-  <meta name="description" content={description} />
-  <meta name="keywords" content={keywords} />
-</svelte:head>
-
+<Metadata />
 <ModeWatcher />
 <Toaster richColors position="top-right" />
 
