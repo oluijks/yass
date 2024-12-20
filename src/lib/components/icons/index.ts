aba22f902a15994b8d956513ceae497e55cd2975
Olaf Luijks <olafluijks@proton.me>
2024-12-20 06:41:36 +0100

    fix: Use Component type in favor of deprecated SvelteComponent

--- src/lib/components/icons/index.ts
+++ src/lib/components/icons/index.ts
@@ -1,11 +1,11 @@
-import type { SvelteComponent } from "svelte";
+import type { Component } from "svelte";
 
 import Bug from "./bug.svelte";
 import Manual from "./manual.svelte";
 import Message from "./message.svelte";
 import Newspaper from "./newspaper.svelte";
 
-export type Icon = SvelteComponent;
+export type Icon = Component;
 
 export const Icons = {
   bug: Bug,
