fe28dc3831715d2257730ec1c691f48800aa3345
Olaf Luijks <olafluijks@proton.me>
2024-12-20 20:12:09 +0100

    feat: Add description and repo url to site config

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -1,19 +1,21 @@
 // Change these values to match your project
 export const siteConfig = {
   name: "Yass",
+  description: "Yet another sveltekit starter",
   url: "http://localhost:5173",
+  repo: "https://github.com/oluijks/yass",
   meta: {
-    title: "site title",
-    description: "site description",
+    title: "Yass",
+    description: "Yet another sveltekit starter",
     keywords: "svelte,sveltekit,drizzle,tailwindcss,sqlite",
   },
   links: {
     social: {
-      github: "https://github.com/oluijks/yass",
-      twitter: "https://twitter.com",
+      github: "https://github.com/oluijks",
+      twitter: "https://x.com",
       facebook: "https://www.facebook.com",
     },
   },
-};
+} as const;
 
 export type SiteConfig = typeof siteConfig;
