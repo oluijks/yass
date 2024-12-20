72bc3a54a66dc4e2e8c7c107d74b7a6355ecf473
Olaf Luijks <olafluijks@proton.me>
2024-12-20 20:10:13 +0100

    refactor: Only enable drizzle logger in dev mode

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,3 +1,4 @@
+import { dev } from "$app/environment";
 import { DATABASE_URL } from "$env/static/private";
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import * as schema from "./schema.js";
@@ -8,6 +9,6 @@ if (!DATABASE_URL)
 export const db = drizzle({
   connection: DATABASE_URL,
   casing: "snake_case",
-  logger: true,
+  logger: dev,
   schema,
 });
