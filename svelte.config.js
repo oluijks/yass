d1fc118789a4ba3b4fc9c24024dd2bdc0a43fc59
Olaf Luijks <olafluijks@proton.me>
2024-12-22 08:41:22 +0100

    feat: Add Dockerfile for locally running test and test production

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import * as child_process from "node:child_process";
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-node";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -14,7 +14,11 @@ const config = {
 
     // @see https://svelte.dev/docs/kit/adapters
     // @see https://svelte.dev/docs/kit/adapter-auto
-    adapter: adapter(),
+    adapter: adapter({
+      precompress: true,
+      out: "build",
+    }),
+
     version: {
       name: child_process.execSync("git rev-parse --short HEAD").toString().trim(),
       // pollInterval: 3000,
